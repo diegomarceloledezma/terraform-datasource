ea190e7a923c2a20697df49056a9a5343d63d714
unknown <diegomarceloledezma@gmail.com>
2025-12-11 07:38:13 -0400

    prueba4

--- main.tf
+++ main.tf
@@ -62,7 +62,7 @@ resource "aws_security_group" "web_sg" {
 
 resource "aws_instance" "web" {
   ami                         = data.aws_ami.amazon_linux.id
-  instance_type               = "t3.small"
+  instance_type               = "t3.micro"
   subnet_id                   = data.aws_subnets.selected.ids[0]
   vpc_security_group_ids      = [aws_security_group.web_sg.id]
   associate_public_ip_address = true
